dc6061da5f3f6805d23f5bc47427546181e1cdea
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-01-02 15:58:50 +0000

    added 404

---
+++ 404.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0" />
+        <link rel="stylesheet" type="text/css" href="assets/css/styles.css" />
+        <title>404 ~ Page not found, sorry!</title>
+    </head>
+    <body class="error">
+        <div id="container">
+            <h1>Page not found</h1>
+            <h3>This is not the page you're looking for...</h3>
+            <iframe src="http://notfound-static.fwebservices.be/404/index.html?&amp;key=74f75fec68aa6108c2459d24ca32c6ea" width="100%" height="650" frameborder="0"></iframe>
+        </div>
+    </body>
+</html>
